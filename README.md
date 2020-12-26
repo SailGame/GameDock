3f1705295e37a9a209c77360c09b0532eef9b901
Jinfeng Liu <917971502@qq.com>
2020-12-26 14:14:39 +0800

    sort out dir structure, add grpc, add clang-format (#1)
    * sort out dir structure, add grpc, add clang-format
    
    * git action and gtest, ctest
    
    * exclude windows

--- README.md
+++ README.md
@@ -4,10 +4,17 @@ GameDock - Dock all games which in your mind
 
 ## Build instructions
 
-~~~bash
+```bash
+git submodule update --init --recursive
 mkdir build
 cd build
 cmake ..
 make
 ./main
-~~~
+```
+
+```bash
+# clang-format
+# version requirement >10
+python3 run-clang-format.py -i -r .
+```
--- main.cpp
+++ main.cpp
@@ -1,24 +1,22 @@
-#include "ftxui/component/menu.hpp"
-#include "include/tetris/tetrisBox.hpp"
- 
 #include <chrono>
+#include <ftxui/component/menu.hpp>
+#include <ftxui/component/screen_interactive.hpp>
 #include <iostream>
+#include <tetris/tetrisBox.hpp>
 #include <thread>
- 
-#include "ftxui/component/screen_interactive.hpp"
- 
+
 using namespace tetris;
 
 int main(int argc, const char* argv[]) {
   using namespace ftxui;
   auto screen = ScreenInteractive::TerminalOutput();
- 
+
   TetrisBox menu;
   menu.entries = {L"entry 1", L"entry 2", L"entry 3"};
   menu.selected = 0;
   menu.on_enter = screen.ExitLoopClosure();
- 
+
   screen.Loop(&menu);
- 
+
   std::cout << "Selected element = " << menu.selected << std::endl;
 }
