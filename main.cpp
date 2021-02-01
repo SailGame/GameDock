79376f2385625673b5e3cc717d266e97788bfafb
Gusabary <gusabary@126.com>
2021-02-01 17:46:24 +0800

    chore: repin to new common lib

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <sailgame/common/game_manager.h>
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/spdlog.h>
-#include <cxxopts.hpp>
 
+#include <cxxopts.hpp>
 #include <iostream>
 
 #include "dock/dock.h"
@@ -16,14 +16,15 @@ using SailGame::Dock::UIProxy;
 int main(int argc, const char* argv[]) {
     cxxopts::Options options("SailGame", "Terminal Game Platform");
 
-    options.add_options()
-        ("c, core", "endpoint of core", cxxopts::value<std::string>());
-    
+    options.add_options()("c, core", "endpoint of core",
+                          cxxopts::value<std::string>());
+
     try {
         auto results = options.parse(argc, argv);
         auto coreAddress = results["core"].as<std::string>();
 
-        spdlog::set_default_logger(spdlog::basic_logger_mt("GameDock", "dev.log"));
+        spdlog::set_default_logger(
+            spdlog::basic_logger_mt("GameDock", "dev.log"));
         spdlog::set_level(spdlog::level::info);
         spdlog::default_logger()->flush_on(spdlog::level::info);
         spdlog::info("hello, game dock!");
@@ -32,8 +33,7 @@ int main(int argc, const char* argv[]) {
             INetworkInterface::CreateStub(coreAddress)));
         Dock dock(uiProxy);
         dock.Loop();
-    }
-    catch (std::exception &e) {
+    } catch (std::exception& e) {
         std::cout << options.help() << std::endl;
         std::cout << e.what() << std::endl;
         std::exit(-1);
